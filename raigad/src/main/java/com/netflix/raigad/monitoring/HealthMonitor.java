766588bfdd3d758ab71c7c19943851fb701523c3
SagarL <sagarit2@gmail.com>
2014-12-02 15:56:34 -0800

    Fixing HealthMonitor

--- raigad/src/main/java/com/netflix/raigad/monitoring/HealthMonitor.java
+++ raigad/src/main/java/com/netflix/raigad/monitoring/HealthMonitor.java
@@ -98,12 +98,11 @@ public class HealthMonitor extends Task
             if(config.isNodeMismatchWithDiscoveryEnabled())
                 //Check if there is Node Mismatch between Discovery and ES
                 healthBean.nodematch = (clusterHealthResponse.getNumberOfNodes() == instanceManager.getAllInstances().size()) ? 0 : 1;
-            else {
+            else
                 healthBean.nodematch = (clusterHealthResponse.getNumberOfNodes() == config.getDesiredNumberOfNodesInCluster()) ? 0 : 1;
 
             if(config.isEurekaHealthCheckEnabled())
                 healthBean.eurekanodematch = (clusterHealthResponse.getNumberOfNodes() == discoveryClient.getApplication(config.getAppName()).getInstances().size()) ? 0 : 1;
-        }
         }
         catch(Exception e)
         {
