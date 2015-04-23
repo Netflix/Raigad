a9cd73eb182f686a6c282dbf1f9cdbeaa55a88cb
Nir Alfasi <alfasi@netflix.com>
2015-04-22 22:44:43 -0700

    fixed typo

--- raigad/src/main/java/com/netflix/raigad/configuration/IConfiguration.java
+++ raigad/src/main/java/com/netflix/raigad/configuration/IConfiguration.java
@@ -41,7 +41,7 @@ public interface IConfiguration
     public String getElasticsearchStartupScript();
 
     /**
-     * @return Path to Elasticsearch stop sript
+     * @return Path to Elasticsearch stop script
      */
     public String getElasticsearchStopScript();
    
