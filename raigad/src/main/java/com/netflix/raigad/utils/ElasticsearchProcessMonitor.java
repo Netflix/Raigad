5822600149e0262d1cb7265771e43d824479dcd6
Nir Alfasi <alfasi@netflix.com>
2015-05-07 23:07:30 -0700

    - Changed ES monitoring process to check if ES is running via REST call to localhost instead of grepping the process from ps -ef - added unit-test

--- raigad/src/main/java/com/netflix/raigad/utils/ElasticsearchProcessMonitor.java
+++ raigad/src/main/java/com/netflix/raigad/utils/ElasticsearchProcessMonitor.java
@@ -15,78 +15,86 @@
  */
 package com.netflix.raigad.utils;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.concurrent.atomic.AtomicBoolean;
-
-import com.netflix.raigad.configuration.IConfiguration;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import com.netflix.raigad.configuration.IConfiguration;
 import com.netflix.raigad.scheduler.SimpleTimer;
 import com.netflix.raigad.scheduler.Task;
 import com.netflix.raigad.scheduler.TaskTimer;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /*
  * This task checks if the Elasticsearch process is running.
  */
 @Singleton
-public class ElasticsearchProcessMonitor extends Task{
+public class ElasticsearchProcessMonitor extends Task {
 
-	public static final String JOBNAME = "ES_MONITOR_THREAD";
+    public static final String JOBNAME = "ES_MONITOR_THREAD";
     private static final Logger logger = LoggerFactory.getLogger(ElasticsearchProcessMonitor.class);
     private static final AtomicBoolean isElasticsearchRunningNow = new AtomicBoolean(false);
     private static final AtomicBoolean wasElasticsearchStarted = new AtomicBoolean(false);
+    private final HttpModule httpModule;
 
     @Inject
-    protected ElasticsearchProcessMonitor(IConfiguration config) {
-		super(config);
-	}
+    protected ElasticsearchProcessMonitor(IConfiguration config, HttpModule httpModule) {
+        super(config);
+        this.httpModule = httpModule;
+    }
 
-	@Override
-	public void execute() throws Exception {
+    @Override
+    public void execute() throws Exception {
 
-        try
-        {
-        		//This returns pid for the Elasticsearch process
-        		Process p = Runtime.getRuntime().exec("pgrep -f " + config.getElasticsearchProcessName());
-        		BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
-            String line = input.readLine();
-        		if (line != null && !isElasticsearchRunning())
-        		{
-        			isElasticsearchRunningNow.set(true);
-                    if (! wasElasticsearchStarted.get()) {
-                        wasElasticsearchStarted.set(true);
-                    }
-        		}
-        		else if(line  == null && isElasticsearchRunning())
-        		{
-        			isElasticsearchRunningNow.set(false);
-        		}
-        }
-        catch(Exception e)
-        {
-        	logger.warn("Exception thrown while checking if Elasticsearch is running or not ", e);
+        try {
+            JSONObject response = getESResponse();
+            Long status = analyzeResponse(response);
+            updateStateHolders(status);
+        } catch (Exception e) {
+            logger.warn("Exception thrown while checking if Elasticsearch is running or not ", e);
             isElasticsearchRunningNow.set(false);
         }
-		
-	}
+    }
 
-    public static TaskTimer getTimer()
-    {
+    protected static Long analyzeResponse(JSONObject response) {
+        Long status = 0L;
+        try {
+            status = (Long)response.get("status");
+        } catch (Exception ex) {
+            logger.warn("Failed to reach localhost due to: " + ex.getLocalizedMessage());
+            status = 500L;
+        }
+        return status;
+    }
+
+    protected JSONObject getESResponse() throws Exception {
+        String URL = httpModule.localhostURL();
+        String RESPONSE = SystemUtils.runHttpGetCommand(URL);
+        return (JSONObject) new JSONParser().parse(RESPONSE);
+    }
+
+    protected void updateStateHolders(Long status) {
+        if (status == 200L && !isElasticsearchRunning()) {
+            isElasticsearchRunningNow.set(true);
+            wasElasticsearchStarted.set(true);
+        } else if (status != 200L && isElasticsearchRunning()) {
+            isElasticsearchRunningNow.set(false);
+        }
+    }
+
+    public static TaskTimer getTimer() {
         return new SimpleTimer(JOBNAME, 10L * 1000);
     }
 
     @Override
-    public String getName()
-    {
+    public String getName() {
         return JOBNAME;
     }
 
-    public static Boolean isElasticsearchRunning()
-    {
+    public static Boolean isElasticsearchRunning() {
         return isElasticsearchRunningNow.get();
     }
 
@@ -95,8 +103,7 @@ public class ElasticsearchProcessMonitor extends Task{
     }
 
     //Added for testing only
-    public static void setElasticsearchStarted()
-    {
-		isElasticsearchRunningNow.set(true);
-	}
+    public static void setElasticsearchStarted() {
+        isElasticsearchRunningNow.set(true);
+    }
 }
